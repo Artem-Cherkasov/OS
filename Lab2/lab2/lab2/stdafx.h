c0ef97c7afed0c69f664ae0369b95458489fc12c
Artem-Cherkasov <79924549+Artem-Cherkasov@users.noreply.github.com>
2022-11-07 16:38:47 +0300

    lab2

---
+++ Lab2/lab2/lab2/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <set>
+#include <iterator>
+#include <unordered_map>
+#include <utility>
